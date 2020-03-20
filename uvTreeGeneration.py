839f904abba48f52c43c5e0c99f3ae7ef824be7f
df-0 <spam@rgl.tv>
2020-03-20 11:40:19 +0000

    first pass

---
+++ uvTreeGeneration.py
@@ -0,0 +1,20 @@
+# Generates uvs for a mesh based on connectivity, useful for tree structures
+
+
+import bpy
+
+
+
+
+def generateConnectivityUVs():
+    # get the selected mesh
+    # starts from the selected vertex
+    # while we have unselected vertices (only works for the first island! so do until we don't have new vertices)
+    # for all unconnected vertices:
+    # if it's connected to any existing vertex
+    # find the closest one
+    pass
+
+
+generateConnectivityUVs()
+print ('Completed program.')
