42fd667934e45f0fd00088eb865ef9f2e29f3cb7
df-0 <spam@rgl.tv>
2020-03-23 09:33:39 +0000

    starting work on dmbp

--- uvTreeGeneration.py
+++ uvTreeGeneration.py
@@ -60,6 +60,11 @@ def generateConnectivityUVs():
     iterations = 0
     generations = [selectedVerts] # set the selected verts to be the first generation
     unconnectedVerts = set(bm.verts)-set(selectedVerts)
+    rootDict = {} # stores the root of each connected vert
+
+    for i in range(len(generations[0])):
+        vert = generations[0][i]
+        rootDict[vert] = (i+1)/(len(generations[0])+1)
 
     # starts from the selected vertex
     # while we have unselected vertices (only works for the first island! so do until we don't have new vertices)
@@ -73,11 +78,12 @@ def generateConnectivityUVs():
             connectionInfo = {} # TODO: this will store vertex, distance pairs
             for edge in edges:
                 for edgeVert in edge.verts:
-                    # if it's connected to any existing vertex
+                    # if it's a new one
                     if edgeVert in unconnectedVerts:
                         # print (edgeVert.index, 'is connected')
                         newGeneration.append(edgeVert)
                         unconnectedVerts.remove(edgeVert)
+                        rootDict[edgeVert] = rootDict[vert] # just copy the root value from the parent
                         # TODO: we could make sure that we're connecting via the shortest route, but that's an optimization for later....
 
         if len(newGeneration) > 0:
@@ -104,17 +110,14 @@ def generateConnectivityUVs():
         generation = generations[iGeneration]
         for iVert in range(len(generation)):
             vert = generation[iVert]
-            # vert.link_loops[0][uv_layer].uv = Vector((iVert/len(generation), iGeneration/len(generations)))
-            # vert.link_loops[0][color_layer] = Vector((iVert/len(generation), iGeneration/len(generations), 0, 1))
-            uvDict[vert] =  Vector(((iVert+1)/(len(generation)+1), (iGeneration+1)/(len(generations)+1)))
-            # print ('setting vert ', vert.index, 'to UV', Vector((iVert/len(generation), iGeneration/len(generations))))
+            uvDict[vert] =  Vector((rootDict[vert], (iGeneration+1)/(len(generations)+1)))
             # UVMap
 
     # now go thru all faces
     for face in bm.faces:
         for loop in face.loops:
             if loop.vert in uvDict:
-                uv = uvDict[loop.vert] 
+                uv = uvDict[loop.vert]
             else:
                 uv = Vector((-1, -1))
             loop[uv_layer].uv = uv
